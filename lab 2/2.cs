0851697515534aac2b4e45a6883bb9c89adf8225
Dawa-Tamaang <63780415+Dawa-Tamaang@users.noreply.github.com>
2023-04-18 21:30:49 +0545

    Update 2.cs

--- lab 2/2.cs
+++ lab 2/2.cs
@@ -1,18 +1,52 @@
 using System;
 
-class Student {
-    public int Id { get; set; }
-    public string Name { get; set; }
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public double Divide(int a, int b)
+    {
+
+        return (double)a / b;
+    }
 }
 
-class Program {
-    static void Main(string[] args) {
-        Student student = new Student() {
-            Id = 191832,
-            Name = "Dawa Tamang"
-        };
-        
-        Console.WriteLine("Student ID: " + student.Id);
-        Console.WriteLine("Student Name: " + student.Name);
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        int a = 6;
+        int b = 7;
+
+        Console.WriteLine("a = " + a + ", b = " + b);
+
+        int sum = calculator.Add(a, b);
+        Console.WriteLine("Sum: " + sum);
+
+        int difference = calculator.Subtract(a, b);
+        Console.WriteLine("Difference: " + difference);
+
+        int product = calculator.Multiply(a, b);
+        Console.WriteLine("Product: " + product);
+
+        double quotient = calculator.Divide(a, b);
+        Console.WriteLine("Quotient: " + quotient);
+
+        Console.ReadLine();
     }
 }
